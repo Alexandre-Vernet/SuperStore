2284d1851c0d7203aa954355763aafc72c25326e
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-30 13:13:50 +0200

    feat(DB): Add connection to postgres

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,31 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+const {
+    POSTGRES_HOST,
+    POSTGRES_PORT,
+    POSTGRES_USERNAME,
+    POSTGRES_PASSWORD,
+    POSTGRES_DATABASE
+} = process.env;
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: POSTGRES_HOST,
+            port: Number(POSTGRES_PORT),
+            username: POSTGRES_USERNAME,
+            password: POSTGRES_PASSWORD,
+            database: POSTGRES_DATABASE,
+            entities: [],
+            ssl: true,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
