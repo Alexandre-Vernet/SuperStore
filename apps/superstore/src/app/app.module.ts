779257e47adf994b87df38d7f5b542952599d8eb
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-05-01 12:03:31 +0200

    feat(list-products): Generate component

--- apps/superstore/src/app/app.module.ts
+++ apps/superstore/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NavbarComponent } from "./navbar/navbar.component";
+import { NavbarComponent } from './navbar/navbar.component';
+import { ListProductsComponent } from "./list-products/list-products.component";
 
 @NgModule({
-    declarations: [AppComponent, NavbarComponent],
+    declarations: [AppComponent, NavbarComponent, ListProductsComponent],
     imports: [
         BrowserModule,
         RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
