0dddb1c34735440b884b306d7df64f3f47375537
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-05-01 20:57:05 +0200

    feat(cart): Generate cart resource

--- apps/superstore/src/app/app.module.ts
+++ apps/superstore/src/app/app.module.ts
@@ -6,14 +6,16 @@ import { appRoutes } from './app.routes';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductModule } from './product/product.module';
 import { HttpClientModule } from '@angular/common/http';
+import { CartModule } from "./cart/cart.module";
 
 @NgModule({
     declarations: [AppComponent, NavbarComponent],
     imports: [
         BrowserModule,
         RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
-        ProductModule,
         HttpClientModule,
+        ProductModule,
+        CartModule
     ],
     providers: [],
     bootstrap: [AppComponent],
---
+++ apps/superstore/src/app/cart/cart.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'superstore-cart',
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.scss'],
+})
+export class CartComponent {
+}
