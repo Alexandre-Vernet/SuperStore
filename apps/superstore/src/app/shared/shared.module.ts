0b627600c6c0c0e32e35b56e76be5553700e1521
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-05-02 16:08:31 +0200

    fix(navbar): Import RouterLink in SharedModule

--- apps/superstore/src/app/shared/shared.module.ts
+++ apps/superstore/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { RouterLink } from "@angular/router";
 
 @NgModule({
     declarations: [NavbarComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink],
     exports: [NavbarComponent]
 })
 export class SharedModule {
