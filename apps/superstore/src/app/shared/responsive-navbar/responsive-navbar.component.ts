a8599f8a067899ec1dd566e163839af9f2209314
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-05-20 18:30:05 +0200

    fix(responsive navbar): Display responsive navbar only onClick hamburger menu

--- apps/superstore/src/app/shared/responsive-navbar/responsive-navbar.component.ts
+++ apps/superstore/src/app/shared/responsive-navbar/responsive-navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AppComponent } from "../../app.component";
 import { Router } from "@angular/router";
+import { UserDto } from "@superstore/libs";
+import { AuthService } from "../../auth/auth.service";
 
 @Component({
     selector: 'superstore-responsive-navbar',
@@ -10,7 +12,8 @@ import { Router } from "@angular/router";
 export class ResponsiveNavbarComponent {
 
     constructor(
-        private router: Router
+        private router: Router,
+        private readonly authService: AuthService,
     ) {
     }
 
@@ -21,5 +24,18 @@ export class ResponsiveNavbarComponent {
     redirectTo(url: string): void {
         this.router.navigateByUrl(url)
             .then(() => AppComponent.displayResponsiveMenu = false)
+    }
+
+    getUserConnected(): UserDto {
+        return this.authService.user;
+    }
+
+    getFirstNameAndLastName(): string {
+        return `${ this.getUserConnected().firstName } ${ this.getUserConnected().lastName }`;
+    }
+
+    signOut(): void {
+        this.authService.signOut();
+        this.router.navigate(['/']);
     }
 }
