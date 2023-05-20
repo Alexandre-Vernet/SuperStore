95cf6d11855ef0cf147ed6e1c44062c53dbd5278
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-05-20 17:49:39 +0200

    feat(responsive navbar): Add router navigate to sign-in & sign-up

--- apps/superstore/src/app/shared/responsive-navbar/responsive-navbar.component.ts
+++ apps/superstore/src/app/shared/responsive-navbar/responsive-navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppComponent } from "../../app.component";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'superstore-responsive-navbar',
@@ -8,7 +9,17 @@ import { AppComponent } from "../../app.component";
 })
 export class ResponsiveNavbarComponent {
 
+    constructor(
+        private router: Router
+    ) {
+    }
+
     toggleResponsiveMenu(): void {
         AppComponent.displayResponsiveMenu = !AppComponent.displayResponsiveMenu;
+    }
+
+    redirectTo(url: string): void {
+        this.router.navigateByUrl(url)
+            .then(() => AppComponent.displayResponsiveMenu = false)
     }
 }
