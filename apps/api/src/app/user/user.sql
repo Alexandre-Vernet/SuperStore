6be1293a6389f1c11e6ea06ac4f6de8133a179bc
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-05-21 23:34:10 +0200

    feat(user): Add column isAdmin

--- apps/api/src/app/user/user.sql
+++ apps/api/src/app/user/user.sql
@@ -5,6 +5,7 @@ CREATE TABLE public.users (
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
