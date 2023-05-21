b4003814d9e39f8287d30cfcd1b9c5f31afd6d3a
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-05-22 00:03:55 +0200

    feat(admin): List products from db

--- apps/superstore/src/app/admin/list-products/list-products.component.ts
+++ apps/superstore/src/app/admin/list-products/list-products.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductDto } from "@superstore/libs";
+import { ProductService } from "../../product/product.service";
+import { NotificationsService } from "../../shared/notifications/notifications.service";
 
 @Component({
-  selector: 'superstore-list-products',
-  templateUrl: './list-products.component.html',
-  styleUrls: ['./list-products.component.scss'],
+    selector: 'superstore-list-products',
+    templateUrl: './list-products.component.html',
+    styleUrls: ['./list-products.component.scss'],
 })
-export class ListProductsComponent {}
+export class ListProductsComponent implements OnInit {
+
+    products: ProductDto[];
+
+    constructor(
+        private readonly productService: ProductService,
+        private readonly notificationService: NotificationsService
+    ) {
+    }
+
+    ngOnInit() {
+        this.productService.getProducts(300, 1)
+            .subscribe({
+                next: ({ products }) => {
+                    this.products = products;
+                }
+            })
+    }
+
+    deleteProduct(product: ProductDto) {
+        this.productService.deleteProduct(product.id)
+            .subscribe({
+                next: () => {
+                    this.products = this.products.filter(p => p.id !== product.id);
+                },
+                error: (err) => {
+                    this.notificationService.showErrorNotification('Error', err.message);
+                }
+            })
+    }
+}
