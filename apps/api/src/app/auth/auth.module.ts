f891d6a0d892aa3bd780219dc05116c13ec02dc8
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-06-09 19:25:55 +0200

    Feat(auth): Verify JWT with middleware

--- apps/api/src/app/auth/auth.module.ts
+++ apps/api/src/app/auth/auth.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "../user/user.entity";
-import { PassportModule } from "@nestjs/passport";
+import { AuthMiddleware } from "./auth.middleware";
+import { OrderController } from "../order/order.controller";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), PassportModule],
-  providers: [AuthService],
-  controllers: [AuthController],
+    imports: [TypeOrmModule.forFeature([User])],
+    providers: [AuthService],
+    controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(AuthMiddleware)
+            .forRoutes(OrderController);
+    }
+}
--- apps/api/src/app/user/user.module.ts
+++ apps/api/src/app/user/user.module.ts
@@ -3,13 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./user.entity";
-import { PassportModule } from "@nestjs/passport";
-import { LocalStrategy } from "./local.strategy";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User]), PassportModule],
+    imports: [TypeOrmModule.forFeature([User])],
     controllers: [UserController],
-    providers: [UserService, LocalStrategy],
+    providers: [UserService],
     exports: [UserService]
 })
 export class UserModule {
