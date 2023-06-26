13c7d9489494ec86c425e8698eb5dac1e5ac4241
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-06-26 23:50:50 +0200

    feat(user) Add / Update / Delete address

--- apps/superstore/src/app/user/address/address.component.ts
+++ apps/superstore/src/app/user/address/address.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AddressDto, deliveryMethods } from "@superstore/interfaces";
+import { AddressDto } from "@superstore/interfaces";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { AddressService } from "../../address/address.service";
 
@@ -11,6 +11,7 @@ import { AddressService } from "../../address/address.service";
 export class AddressComponent implements OnInit {
 
     addresses: AddressDto[] = [];
+    selectedAddress: AddressDto;
     formAddress = new FormGroup({
         company: new FormControl(),
         address: new FormControl('', [Validators.required]),
@@ -19,12 +20,10 @@ export class AddressComponent implements OnInit {
         city: new FormControl('', [Validators.required]),
         zipCode: new FormControl('', [Validators.required]),
         phone: new FormControl('', [Validators.required]),
-        deliveryMethod: new FormControl('', [Validators.required]),
-        paymentMethod: new FormControl('CB', [Validators.required]),
     });
 
     constructor(
-        private readonly addressService: AddressService
+        private readonly addressService: AddressService,
     ) {
     }
 
@@ -32,21 +31,74 @@ export class AddressComponent implements OnInit {
         this.addressService.getUserAddresses()
             .subscribe(addresses => {
                 this.addresses = addresses;
+                this.selectedAddress = addresses[0];
+
+                this.formAddress.patchValue({
+                    company: addresses[0].company,
+                    address: addresses[0]?.address,
+                    apartment: addresses[0].apartment,
+                    country: addresses[0]?.country,
+                    city: addresses[0]?.city,
+                    zipCode: addresses[0]?.zipCode,
+                    phone: addresses[0]?.phone,
+                });
             });
     }
 
     clearFormAddress() {
         this.formAddress.reset();
+    }
+
+    selectAddress(address: AddressDto) {
+        this.selectedAddress = address;
         this.formAddress.patchValue({
-            paymentMethod: 'CB',
-            deliveryMethod: deliveryMethods[0].name,
+            company: address.company,
+            address: address.address,
+            apartment: address.apartment,
+            country: address.country,
+            city: address.city,
+            zipCode: address.zipCode,
+            phone: address.phone,
         });
+    }
+
+    submitForm() {
+        if (this.formAddress.valid) {
+            this.createAddress();
+        }
+    }
+
+    createAddress() {
+        const address = this.formAddress.value;
+
+        this.addressService
+            .createAddress({
+                company: address.company,
+                address: address.address,
+                apartment: address.apartment,
+                country: address.country,
+                city: address.city,
+                zipCode: address.zipCode,
+                phone: address.phone
+            })
+            .subscribe((newAddress) => {
+                if (this.addresses.length === 0) {
+                    this.addresses.push(newAddress);
+                    this.selectAddress(newAddress);
+                    return;
+                }
+                this.addresses.find(a => a.id === newAddress.id) ?
+                    this.addresses[this.addresses.findIndex(a => a.id === newAddress.id)] = newAddress :
+                    this.addresses.push(newAddress);
+            });
     }
 
     removeAddress(address: AddressDto) {
         this.addressService.deleteAddress(address)
             .subscribe(() => {
                 this.addresses = this.addresses.filter(a => a.id !== address.id);
+                this.clearFormAddress();
+                this.selectAddress(this.addresses[0]);
             });
     }
 }
