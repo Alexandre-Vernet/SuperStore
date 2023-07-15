913de5e643ee467772f0abb00577f550727f2d31
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-07-15 14:47:12 +0200

    feat(shipping): Add component

--- apps/superstore/src/app/customer-service/customer-service-routing.module.ts
+++ apps/superstore/src/app/customer-service/customer-service-routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { CustomerServiceComponent } from "./customer-service.component";
 import { ContactComponent } from "./contact/contact.component";
 import { FaqComponent } from "./faq/faq.component";
+import { ShippingComponent } from "./shipping/shipping.component";
 
 const routes: Routes = [
     {
         path: '',
         component: CustomerServiceComponent,
         children: [
+            {
+                path: 'shipping',
+                component: ShippingComponent
+            },
             {
                 path: 'contact',
                 component: ContactComponent
--- apps/superstore/src/app/customer-service/customer-service.module.ts
+++ apps/superstore/src/app/customer-service/customer-service.module.ts
@@ -3,17 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerServiceRoutingModule } from './customer-service-routing.module';
 import { ContactComponent } from './contact/contact.component';
-import { CustomerServiceComponent } from "./customer-service.component";
-import { ReactiveFormsModule } from "@angular/forms";
-import { FaqComponent } from "./faq/faq.component";
+import { CustomerServiceComponent } from './customer-service.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FaqComponent } from './faq/faq.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
-    declarations: [
-        CustomerServiceComponent,
-        ContactComponent,
-        FaqComponent
-    ],
-    imports: [CommonModule, CustomerServiceRoutingModule, ReactiveFormsModule],
+  declarations: [
+    CustomerServiceComponent,
+    ContactComponent,
+    FaqComponent,
+    ShippingComponent,
+  ],
+  imports: [CommonModule, CustomerServiceRoutingModule, ReactiveFormsModule],
 })
-export class CustomerServiceModule {
-}
+export class CustomerServiceModule {}
