f5ef5b2ad1c1e83e40827e364beb0e002513c2b8
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-02-04 00:20:56 +0100

    feat(product-promotion): Add pagination to list-promotion

--- apps/superstore/src/app/admin/promotions-code/list-promotions/list-promotions.component.ts
+++ apps/superstore/src/app/admin/promotions-code/list-promotions/list-promotions.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { PromotionDto } from "@superstore/interfaces";
-import { PromotionService } from "../../../promotion/promotion.service";
-import { SearchBar } from "../../search-bar/search-bar";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PromotionDto } from '@superstore/interfaces';
+import { PromotionService } from '../../../promotion/promotion.service';
+import { BehaviorSubject, combineLatest, Subject, takeUntil, tap } from 'rxjs';
+import { AdminSearchBarComponent } from '../../search-bar/admin-search-bar/admin-search-bar.component';
 
 @Component({
     selector: 'superstore-list-promotions',
     templateUrl: './list-promotions.component.html',
-    styleUrls: ['./list-promotions.component.scss'],
+    styleUrls: ['./list-promotions.component.scss']
 })
-export class ListPromotionsComponent implements OnInit {
+export class ListPromotionsComponent implements OnInit, OnDestroy {
     promotions: PromotionDto[] = [];
+    filteredPromotions: PromotionDto[] = [];
     editedPromotion: PromotionDto;
-    searchBar = '';
+
     showModalEditPromotion = false;
+
+    pagination = {
+        currentPage: new BehaviorSubject<number>(1),
+        itemsPerPage: 25,
+        totalPage: new BehaviorSubject<number>(0)
+    };
+
+    unsubscribe$ = new Subject<void>();
 
     constructor(
         private readonly promotionService: PromotionService
@@ -20,16 +30,35 @@ export class ListPromotionsComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.promotionService.promotions$
-            .subscribe((promotions) => {
-                promotions.sort((a, b) => a?.id - b?.id);
+        combineLatest([
+            this.promotionService.promotions$,
+            AdminSearchBarComponent.searchBar
+        ])
+            .pipe(
+                takeUntil(this.unsubscribe$),
+                tap(([promotions]) => promotions.sort((a, b) => a?.id - b?.id)),
+                tap(([promotions]) => this.pagination.totalPage.next(Math.ceil(promotions.length / this.pagination.itemsPerPage)))
+            )
+            .subscribe(([promotions, search]) => {
                 this.promotions = promotions;
-            });
 
-        SearchBar.searchBar
-            .subscribe((search) => {
-                this.searchBar = search;
+                if (search) {
+                    this.filteredPromotions = promotions.filter(promotion =>
+                        promotion.amount.toString().includes(search.toLowerCase()) ||
+                        promotion.label.toLowerCase().includes(search.toLowerCase())
+                    );
+                }
+
+                if (this.filteredPromotions.length <= 0 || !search) {
+                    this.filteredPromotions = [...this.promotions];
+                }
+                this.pagination.currentPage.next(1);
             });
+    }
+
+    ngOnDestroy() {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
     }
 
     openModalEditPromotion() {
@@ -49,5 +78,9 @@ export class ListPromotionsComponent implements OnInit {
     deletePromotion(promotion: PromotionDto) {
         this.promotionService.deletePromotion(promotion)
             .subscribe();
+    }
+
+    pageChange(page: number) {
+        this.pagination.currentPage.next(page);
     }
 }
