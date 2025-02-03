af0177ae2ce8ae864953b70906591b480f868dfe
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-02-04 00:21:04 +0100

    feat(product-users): Add pagination to list-users

--- apps/superstore/src/app/admin/users/list-users/list-users.component.ts
+++ apps/superstore/src/app/admin/users/list-users/list-users.component.ts
@@ -1,19 +1,29 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import { UserService } from "../../../user/user.service";
-import { UserDto } from "@superstore/interfaces";
-import { SearchBar } from "../../search-bar/search-bar";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from '../../../user/user.service';
+import { UserDto } from '@superstore/interfaces';
+import { BehaviorSubject, combineLatest, Subject, takeUntil, tap } from 'rxjs';
+import { AdminSearchBarComponent } from '../../search-bar/admin-search-bar/admin-search-bar.component';
 
 @Component({
     selector: 'superstore-users',
     templateUrl: './list-users.component.html',
-    styleUrls: ['./list-users.component.scss'],
+    styleUrls: ['./list-users.component.scss']
 })
-export class ListUsersComponent implements OnInit {
+export class ListUsersComponent implements OnInit, OnDestroy {
 
     users: UserDto[] = [];
+    filteredUsers: UserDto[] = [];
     editedUser: UserDto;
-    searchBar = '';
+
     showModalAddProduct = false;
+
+    pagination = {
+        currentPage: new BehaviorSubject<number>(1),
+        itemsPerPage: 25,
+        totalPage: new BehaviorSubject<number>(0)
+    };
+
+    unsubscribe$ = new Subject<void>();
 
     constructor(
         private readonly userService: UserService
@@ -21,15 +31,36 @@ export class ListUsersComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.userService.users$
-            .subscribe((users) => {
+        combineLatest([
+            this.userService.users$,
+            AdminSearchBarComponent.searchBar
+        ])
+            .pipe(
+                takeUntil(this.unsubscribe$),
+                tap(([users]) => users.sort((a, b) => a?.id - b?.id)),
+                tap(([users]) => this.pagination.totalPage.next(Math.ceil(users.length / this.pagination.itemsPerPage)))
+            )
+            .subscribe(([users, search]) => {
                 this.users = users;
-            });
 
-        SearchBar.searchBar
-            .subscribe((search) => {
-                this.searchBar = search;
+                if (search) {
+                    this.filteredUsers = users.filter(user =>
+                        user.firstName.toLowerCase().includes(search.toLowerCase()) ||
+                        user.lastName.toLowerCase().includes(search.toLowerCase()) ||
+                        user.email.toLowerCase().includes(search.toLowerCase())
+                    );
+                }
+
+                if (this.filteredUsers.length <= 0 || !search) {
+                    this.filteredUsers = [...this.users];
+                }
+                this.pagination.currentPage.next(1);
             });
+    }
+
+    ngOnDestroy() {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
     }
 
 
@@ -51,10 +82,7 @@ export class ListUsersComponent implements OnInit {
         this.showModalAddProduct = false;
     }
 
-    // Escape key to clear search bar
-    @HostListener('document:keydown.escape', ['$event']) onKeydownHandler() {
-        if (this.searchBar.length > 0) {
-            this.searchBar = '';
-        }
+    pageChange(page: number) {
+        this.pagination.currentPage.next(page);
     }
 }
