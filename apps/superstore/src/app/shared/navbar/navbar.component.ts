6275f8ebedad09d44c2f251564ef035d3a7ca547
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-02-23 17:28:38 +0100

    clean(navbar): Add listener window:resize

--- apps/superstore/src/app/shared/navbar/navbar.component.ts
+++ apps/superstore/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { AppComponent } from '../../app.component';
 import { OrderProductDto } from '@superstore/interfaces';
 
@@ -11,10 +11,15 @@ export class NavbarComponent {
 
     cart: OrderProductDto[] = [];
 
-    protected readonly window = window;
+    isResponsive = window.innerWidth < 1023;
 
 
     toggleResponsiveMenu(): void {
         AppComponent.displayResponsiveMenu = !AppComponent.displayResponsiveMenu;
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize() {
+        this.isResponsive = window.innerWidth < 1023;
     }
 }
