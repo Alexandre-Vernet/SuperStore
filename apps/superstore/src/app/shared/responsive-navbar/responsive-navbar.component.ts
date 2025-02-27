62aebfd3c03ce3f9e1643277cb11539a45a13faa
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-02-27 17:45:21 +0100

    feat(menu): Add animation open responsive menu

--- apps/superstore/src/app/shared/responsive-navbar/responsive-navbar.component.ts
+++ apps/superstore/src/app/shared/responsive-navbar/responsive-navbar.component.ts
@@ -1,11 +1,23 @@
 import { Component, ElementRef, HostListener, Input, Output, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
     selector: 'superstore-responsive-navbar',
     templateUrl: './responsive-navbar.component.html',
-    styleUrls: ['./responsive-navbar.component.scss']
+    styleUrls: ['./responsive-navbar.component.scss'],
+    animations: [
+        trigger('slideInOut', [
+            transition(':enter', [
+                style({ transform: 'translateX(-100%)', opacity: 0 }),
+                animate('200ms ease-out', style({ transform: 'translateX(0)', opacity: 1 }))
+            ]),
+            transition(':leave', [
+                animate('200ms ease-in', style({ transform: 'translateX(-100%)', opacity: 0 }))
+            ])
+        ]),
+    ]
 })
 export class ResponsiveNavbarComponent {
 
